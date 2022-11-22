f496a1573b3dc4e543e4cb563c4988d1026f6708
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2022-11-22 18:03:51 +0000

    Part 4

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import app from './server';
 
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+// const PORT = 3000;
+// get port from .env file or use 3000
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}`);
